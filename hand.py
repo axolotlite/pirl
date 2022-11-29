ae649d18b5b30866626657f65692014d7117090a
rogitson <omarm.elaraby2000@gmail.com>
2022-11-29 16:36:38 +0200

    ✨feature: added sidecam

--- hand.py
+++ hand.py
@@ -3,66 +3,28 @@ import mediapipe as mp
 import numpy as np
 import threading
 import mouse
-import voice
+# import voice
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
 points = []
 count = 0
+threshold = 0
+thresFlag = False
+right = False
 h = None
 camIdx = 0
+sideIdx = 1
 screen_width = 1920
 screen_height = 1080
+smoothFactor = 13
 
 
 def main():
-    # For static images:
-    IMAGE_FILES = []
-    with mp_hands.Hands(
-            static_image_mode=True,
-            max_num_hands=2,
-            min_detection_confidence=0.5) as hands:
-        for idx, file in enumerate(IMAGE_FILES):
-            # Read an image, flip it around y-axis for correct handedness output (see
-            # above).
-            image = cv2.flip(cv2.imread(file), 1)
-            # Convert the BGR image to RGB before processing.
-            results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-            # Print handedness and draw hand landmarks on the image.
-            print('Handedness:', results.multi_handedness)
-            if not results.multi_hand_landmarks:
-                continue
-            image_height, image_width, _ = image.shape
-            annotated_image = image.copy()
-            for hand_landmarks in results.multi_hand_landmarks:
-                print('hand_landmarks:', hand_landmarks)
-                print(
-                    f'Index finger tip coordinates: (',
-                    f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
-                    f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
-                )
-                mp_drawing.draw_landmarks(
-                    annotated_image,
-                    hand_landmarks,
-                    mp_hands.HAND_CONNECTIONS,
-                    mp_drawing_styles.get_default_hand_landmarks_style(),
-                    mp_drawing_styles.get_default_hand_connections_style())
-            cv2.imwrite(
-                '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
-            # Draw hand world landmarks.
-            if not results.multi_hand_world_landmarks:
-                continue
-            for hand_world_landmarks in results.multi_hand_world_landmarks:
-                mp_drawing.plot_landmarks(
-                    hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
-
-    # For webcam input:
     cap = cv2.VideoCapture(camIdx)
     cap_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     cap_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    print(cap_width, cap_height)
 
     global points, screen_width, screen_height
     points = calibrate(cap)
@@ -99,13 +61,66 @@ def main():
                         # Tip of pointer finger only
                         if idx != 8:
                             continue
-                        cx, cy = landmark.x * cap_width, landmark.y*cap_height
+                        cx, cy = landmark.x * cap_width, landmark.y * cap_height
                         cx, cy = normalizePoint(cx, cy)
                         if 0 <= cx < screen_width and 0 <= cy < screen_height:
-                            mouse.move(cx, cy)
+                            mouse.move((cx//smoothFactor)*smoothFactor,
+                                       (cy//smoothFactor)*smoothFactor)
             # Flip the image horizontally for a selfie-view display.
             # cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
             cv2.imshow('MediaPipe Hands', normalizeImg(image))
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+    cap.release()
+
+
+def side():
+    cap = cv2.VideoCapture(sideIdx)
+    cap_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    cap_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    global threshold
+    calibrateSide(cap)
+
+    with mp_hands.Hands(
+            model_complexity=0,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5) as hands:
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+                # If loading a video, use 'break' instead of 'continue'.
+                continue
+
+            # To improve performance, optionally mark the image as not writeable to
+            # pass by reference.
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = hands.process(image)
+
+            # Draw the hand annotations on the image.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(
+                        image,
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS,
+                        mp_drawing_styles.get_default_hand_landmarks_style(),
+                        mp_drawing_styles.get_default_hand_connections_style())
+                    for idx, landmark in enumerate(hand_landmarks.landmark):
+                        # Tip of pointer finger only
+                        if idx != 8:
+                            continue
+                        if (right and landmark.x*cap_width >= threshold) or (not right and landmark.x*cap_width <= threshold):
+                            mouse.press()
+                        else:
+                            mouse.release()
+            # Flip the image horizontally for a selfie-view display.
+            # cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+            cv2.imshow('Side View', image)
             if cv2.waitKey(5) & 0xFF == 27:
                 break
     cap.release()
@@ -167,6 +182,39 @@ def calibrate(cap):
     return points
 
 
+def sideMouse(event, x, y, flags, params):
+    global threshold, thresFlag
+    if event == cv2.EVENT_LBUTTONDOWN:
+        thresFlag = True
+    elif event == cv2.EVENT_MOUSEMOVE:
+        threshold = x
+
+
+def calibrateSide(cap):
+    waitTime = 50
+
+    while (cap.isOpened()):
+
+        grabbed, frame = cap.read()
+
+        cv2.namedWindow('Side Calibration')
+        cv2.setMouseCallback('Side Calibration', sideMouse)
+
+        if thresFlag:
+            break
+        
+        if threshold != 0:
+            cv2.line(frame, [threshold, 0], [threshold, screen_height], (187, 87, 231), 2)
+
+        cv2.imshow('Side Calibration', frame)
+        key = cv2.waitKey(waitTime)
+
+        if key == ord('b'):
+            break
+
+    cv2.destroyWindow('Side Calibration')
+
+
 def getHomography(points):
     global h, screen_width, screen_height
     pts_src = np.array(points)
@@ -193,6 +241,11 @@ def normalizePoint(x, y):
 
 
 if __name__ == "__main__":
-    # thread = threading.Thread(target=voice.main)
-    # thread.start()
-    main()
+    # voice = threading.Thread(target=voice.main)
+    # voice.start()
+    main = threading.Thread(target=main)
+    side = threading.Thread(target=side)
+    main.start()
+    side.start()
+    main.join()
+    side.join()
