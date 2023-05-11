b7b98e8505fcdb56a7e6b45f6a3e72737f148d8f
rogitson <omarm.elaraby2000@gmail.com>
2023-05-11 20:21:56 +0300

    refactored homography and hand recognition code

---
+++ utils/homography.py
@@ -0,0 +1,290 @@
+import cv2
+import numpy as np
+from screeninfo import get_monitors
+
+
+class Homography(object):
+    def __init__(self) -> None:
+        self.points = []
+        self.count = 0
+        self.h_matrix = None
+        self.pmon = get_monitors()[0]
+        self.s_width, self.s_height = self.pmon.width, self.pmon.height
+        
+    def on_mouse(self, event, x, y, flags, params):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            if self.count < 4:
+                if self.count == 0:
+                    self.points.append([x, y])
+                if self.count < 3:
+                    self.points.append([x, y])
+                self.points[self.count] = [x, y]
+                self.count += 1
+        elif event == cv2.EVENT_MOUSEMOVE:
+            if self.count == 0 or self.count == 4:
+                pass
+            else:
+                self.points[self.count] = [x, y]
+
+
+    def calibrate(self, cap):
+        waitTime = 50
+
+        while (cap.isOpened()):
+
+            success, frame = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+                # If loading a video, use 'break' instead of 'continue'.
+                continue
+
+            cv2.namedWindow('Calibration')
+            cv2.setMouseCallback('Calibration', self.on_mouse)
+
+            if self.count == 4:
+                break
+
+            for i in range(len(self.points)):
+                if (i + 1 == len(self.points)):
+                    cv2.line(frame, self.points[i], self.points[0], (187, 87, 231), 2)
+                else:
+                    cv2.line(frame, self.points[i], self.points[i+1], (187, 87, 231), 2)
+
+            cv2.imshow('Calibration', frame)
+
+            key = cv2.waitKey(waitTime)
+
+            if key == ord('b'):
+                break
+            elif key == ord('r'):
+                self.count = 0
+                self.points = []
+
+        cv2.destroyWindow('Calibration')
+        
+        pts_src = np.array(self.points)
+        pts_dst = np.array([[0, 0], [0, self.s_height], [
+                        self.s_width, self.s_height], [self.s_width, 0]])
+
+        # Calculate Homography
+        self.h_matrix, _ = cv2.findHomography(pts_src, pts_dst)
+
+
+    def normalizeImg(self, img):
+        # Warp source image to destination based on homography
+        return cv2.warpPerspective(
+            img, self.h_matrix, (self.s_width, self.s_height))
+
+
+    def normalizePoint(self, x, y):
+        pts = np.dot(self.h_matrix, np.array([x, y, 1.0]))
+        pts = pts/pts[-1]
+        return int(pts[0]), int(pts[1])
+
+
+    def dejitter(self, x, y):
+        jitVarX = self.s_width // 100
+        jitVarY = self.s_height // 100
+        x = (x // jitVarX) * jitVarX
+        y = (y // jitVarY) * jitVarY
+        return x, y
+
+
+def main():
+    pass
+    # startx, starty = 0, 0
+
+    # cap = cv2.VideoCapture(camIdx)
+    # cap_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    # cap_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    # # FPS Measurement ########################################################
+    # cvFpsCalc = CvFpsCalc(buffer_len=10)
+    
+    # h = Homography()
+    # h.calibrate(cap)
+
+    # keypoint_classifier = KeyPointClassifier()
+    # # Read labels ###########################################################
+    # with open('model/keypoint_classifier/keypoint_classifier_label.csv',
+    #           encoding='utf-8-sig') as f:
+    #     keypoint_classifier_labels = csv.reader(f)
+    #     keypoint_classifier_labels = [
+    #         row[0] for row in keypoint_classifier_labels
+    #     ]
+
+    # with mp_hands.Hands(
+    #         model_complexity=0,
+    #         min_detection_confidence=0.5,
+    #         min_tracking_confidence=0.5) as hands:
+    #     while cap.isOpened():
+    #         fps = cvFpsCalc.get()
+
+    #         success, image = cap.read()
+    #         if not success:
+    #             print("Ignoring empty camera frame.")
+    #             # If loading a video, use 'break' instead of 'continue'.
+    #             continue
+
+    #         debug_image = copy.deepcopy(image)
+    #         # To improve performance, optionally mark the image as not writeable to
+    #         # pass by reference.
+    #         image.flags.writeable = False
+    #         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    #         results = hands.process(image)
+
+    #         # Draw the hand annotations on the image.
+    #         image.flags.writeable = True
+    #         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    #         if results.multi_hand_landmarks:
+    #             for hand_landmarks in results.multi_hand_landmarks:
+    #                 mp_drawing.draw_landmarks(
+    #                     image,
+    #                     hand_landmarks,
+    #                     mp_hands.HAND_CONNECTIONS,
+    #                     mp_drawing_styles.get_default_hand_landmarks_style(),
+    #                     mp_drawing_styles.get_default_hand_connections_style())
+    #                 # Landmark calculation
+    #                 landmark_list = calc_landmark_list(
+    #                     debug_image, hand_landmarks)
+    #                 # Conversion to relative coordinates / normalized coordinates
+    #                 pre_processed_landmark_list = pre_process_landmark(
+    #                     landmark_list)
+    #                 # Hand sign classification
+    #                 hand_sign_id = keypoint_classifier(
+    #                     pre_processed_landmark_list)
+    #                 if hand_sign_id == 2 or hand_sign_id == 3:  # Point gesture
+    #                     for idx, landmark in enumerate(hand_landmarks.landmark):
+    #                         # Tip of pointer finger only
+    #                         if idx != 8:
+    #                             continue
+    #                         cx, cy = landmark.x * cap_width, landmark.y*cap_height
+    #                         cx, cy = h.normalizePoint(cx, cy)
+    #                         cx, cy = h.dejitter(cx, cy)
+    #                         cx = constrainVal(cx, h.s_width)
+    #                         cy = constrainVal(cy, h.s_height)
+    #                         # mouse.move(cx, cy)
+    #                         wind_mouse(startx, starty, cx, cy,
+    #                                    move_mouse=lambda x, y: mouse.move(x, y))
+    #                         startx, starty = cx, cy
+    #                     if hand_sign_id == 3:
+    #                         mouse.press()
+    #                     else:
+    #                         mouse.release()
+    #                 else:
+    #                     pass
+
+    #         image = h.normalizeImg(image)
+    #         image = draw_fps(image, fps)
+    #         cv2.imshow('MediaPipe Hands', image)
+    #         if cv2.waitKey(5) & 0xFF == 27:
+    #             break
+    # cap.release()
+
+
+# def constrainVal(val, max, min=0):
+#     val = val if val >= min else min
+#     val = val if val < max else max
+#     return val
+
+
+# def wind_mouse(start_x, start_y, dest_x, dest_y, G_0=9, W_0=3, M_0=15, D_0=12, move_mouse=lambda x, y: None):
+#     '''
+#     WindMouse algorithm. Calls the move_mouse kwarg with each new step.
+#     Released under the terms of the GPLv3 license.
+#     G_0 - magnitude of the gravitational fornce
+#     W_0 - magnitude of the wind force fluctuations
+#     M_0 - maximum step size (velocity clip threshold)
+#     D_0 - distance where wind behavior changes from random to damped
+#     '''
+#     sqrt3 = np.sqrt(3)
+#     sqrt5 = np.sqrt(5)
+
+#     current_x, current_y = start_x, start_y
+#     v_x = v_y = W_x = W_y = 0
+#     dist = np.hypot(dest_x-start_x, dest_y-start_y)
+#     while dist >= 1:
+#         W_mag = min(W_0, dist)
+#         if dist >= D_0:
+#             W_x = W_x/sqrt3 + (2*np.random.random()-1)*W_mag/sqrt5
+#             W_y = W_y/sqrt3 + (2*np.random.random()-1)*W_mag/sqrt5
+#         else:
+#             W_x /= sqrt3
+#             W_y /= sqrt3
+#             if M_0 < 3:
+#                 M_0 = np.random.random()*3 + 3
+#             else:
+#                 M_0 /= sqrt5
+#         v_x += W_x + G_0*(dest_x-start_x)/dist
+#         v_y += W_y + G_0*(dest_y-start_y)/dist
+#         v_mag = np.hypot(v_x, v_y)
+#         if v_mag > M_0:
+#             v_clip = M_0/2 + np.random.random()*M_0/2
+#             v_x = (v_x/v_mag) * v_clip
+#             v_y = (v_y/v_mag) * v_clip
+#         start_x += v_x
+#         start_y += v_y
+#         move_x = int(np.round(start_x))
+#         move_y = int(np.round(start_y))
+#         if current_x != move_x or current_y != move_y:
+#             # This should wait for the mouse polling interval
+#             current_x = move_x
+#             current_y = move_y
+#             move_mouse(current_x, current_y)
+#         dist = np.hypot(dest_x-start_x, dest_y-start_y)
+#     return current_x, current_y
+
+
+# def draw_fps(image, fps):
+#     cv2.putText(image, "FPS:" + str(fps), (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+#                 1.0, (0, 0, 0), 4, cv2.LINE_AA)
+#     cv2.putText(image, "FPS:" + str(fps), (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+#                 1.0, (255, 255, 255), 2, cv2.LINE_AA)
+#     return image
+
+
+# def calc_landmark_list(image, landmarks):
+#     image_width, image_height = image.shape[1], image.shape[0]
+
+#     landmark_point = []
+
+#     # Keypoint
+#     for _, landmark in enumerate(landmarks.landmark):
+#         landmark_x = min(int(landmark.x * image_width), image_width - 1)
+#         landmark_y = min(int(landmark.y * image_height), image_height - 1)
+#         # landmark_z = landmark.z
+
+#         landmark_point.append([landmark_x, landmark_y])
+
+#     return landmark_point
+
+
+# def pre_process_landmark(landmark_list):
+#     temp_landmark_list = copy.deepcopy(landmark_list)
+
+#     # Convert to relative coordinates
+#     base_x, base_y = 0, 0
+#     for index, landmark_point in enumerate(temp_landmark_list):
+#         if index == 0:
+#             base_x, base_y = landmark_point[0], landmark_point[1]
+
+#         temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
+#         temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
+
+#     # Convert to a one-dimensional list
+#     temp_landmark_list = list(
+#         itertools.chain.from_iterable(temp_landmark_list))
+
+#     # Normalization
+#     max_value = max(list(map(abs, temp_landmark_list)))
+
+#     def normalize_(n):
+#         return n / max_value
+
+#     temp_landmark_list = list(map(normalize_, temp_landmark_list))
+
+#     return temp_landmark_list
+
+
+if __name__ == "__main__":
+    main()
