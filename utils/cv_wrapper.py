07223d3a7c736b612abcd1fd2f9c6ce22afcac18
Ahmed Said <ahmed.el.sayeed1914@gmail.com>
2023-05-13 11:08:48 +0200

    add cv_wrapper util

---
+++ utils/cv_wrapper.py
@@ -0,0 +1,17 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QGridLayout, QWidget
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtCore import Qt
+import cv2
+"""
+This file contains our wrappers for opencv,
+it mainly converts opencv related outputs into pyqt compatible format.
+"""
+
+def convert_image(cv_img,display_width=640, display_height=480):
+    """Convert from an opencv image to QPixmap"""
+    rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+    h, w, ch = rgb_image.shape
+    bytes_per_line = ch * w
+    convert_to_Qt_format = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
+    p = convert_to_Qt_format.scaled(display_width, display_height, Qt.KeepAspectRatio)
+    return QPixmap.fromImage(p)
